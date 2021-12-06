2b0067a9c2a951d5cbac418a9b71176de2b9946e
Emerson <emso.alb@gmail.com>
2021-12-06 16:23:10 -0500

    primeiro commit

---
+++ 1_exercicios/scripts.js
@@ -0,0 +1,25 @@
+console.log('\n-----1----');
+console.log("Emerson");
+console.log('Antonio');
+console.log(`A soma de 5 + 5 é ${5+5}`);
+
+console.log('\n-----2----');
+console.log(5);
+console.log(5.5);
+console.log(5+1);
+
+console.log('\n-----3----');
+console.log(5 > 3);
+console.log(3 < 5);
+console.log(5 == '5');
+console.log(5 != 3);
+console.log(5 === 5);
+
+console.log('\n-----4----');
+console.log(5 > 4 && 'emerson' == 'emerson');
+console.log(5 == 5 || 5 <= 4);
+console.log(!(false));
+
+console.log('\n-----5----');
+console.log('emerson' - 5);
+console.log('emerson' + 5);
