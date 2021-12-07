068a5dc42bb02f6c6047d865fb64ec9b71b33b2a
Emerson <emso.alb@gmail.com>
2021-12-07 14:12:54 -0500

    att

---
+++ secao_5_exercicios/exercicio_11/scripts.js
@@ -0,0 +1,9 @@
+//velocidade if/else
+
+let velocidade = prompt("Qual a velocidade atual?");
+
+if (velocidade > 80) {
+    console.log("Reduza!! Você está acima da velocidade máxima permitida!!");
+} else {
+    console.log("Muito bem, velocidade OK!");
+}
