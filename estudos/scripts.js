83ac805403333b2c1c54ce30328a5581f7e091a9
Emerson <emso.alb@gmail.com>
2021-12-09 08:36:38 -0500

    estudo

---
+++ estudos/scripts.js
@@ -0,0 +1,11 @@
+function imprimirNoConsole() { 
+    console.log("Olá mundo!");
+}
+
+imprimirNoConsole();
+
+function imprimirUmNumero(num) {
+    console.log("O número é: " + num);
+}
+
+imprimirUmNumero(2);
