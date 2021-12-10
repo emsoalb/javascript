a48a63dd10ccafecb4af87fa6edd1b966fcf4d92
Emerson <emso.alb@gmail.com>
2021-12-10 10:33:31 -0500

    estudos

--- estudos/scripts.js
+++ estudos/scripts.js
@@ -1,4 +1,4 @@
-function imprimirNoConsole() { 
+function imprimirNoConsole() {
     console.log("Olá mundo!");
 }
 
@@ -9,3 +9,56 @@ function imprimirUmNumero(num) {
 }
 
 imprimirUmNumero(2);
+imprimirUmNumero(4);
+imprimirUmNumero(6);
+imprimirUmNumero(8);
+
+const numeroAleatorio = function () {
+    return Math.random();
+}
+
+numeroAleatorio();
+numeroAleatorio();
+
+console.log("este tá na var " + numeroAleatorio());
+
+function multiplicarTresNumeros(x, y, z) {
+    return x * y * z;
+}
+console.log(multiplicarTresNumeros(2, 3, 4));
+
+const mult = multiplicarTresNumeros(5, 4, 8);
+
+console.log("O valor de mult é " + mult);
+
+function podeDirigir(idade, cnh) {
+    if (idade >= 18 && cnh == true) {
+        console.log("Pode dirigir");
+    } else {
+        console.log("Não pode dirigir");
+    }
+}
+
+podeDirigir(19,true);
+podeDirigir(25,true);
+podeDirigir(44,0);
+podeDirigir(19,1);
+podeDirigir(17,false);
+
+let y = 10;
+
+function imprimir(){
+    let y = 150;
+    console.log(y);
+}
+imprimir();
+console.log(y);
+
+let x = 10;
+if (x > 5) {
+    let x = 20;
+    x++;
+    console.log(x);
+}
+
+console.log(x);
